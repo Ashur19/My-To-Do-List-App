4796eb1f3b4f529399ce8ac54f8e0025091aa352
Ashur19 <iglesiasjota1911@gmail.com>
2023-02-07 12:51:12 +0100

    3rd Commit

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const CheckBox = () => {
+const CheckBox = props => {
+    const {
+        onChange,
+        data: { id, Description, done }
+    } = props
     return (
-        <div>
-            CheckBox
-        </div>
+        <Fragment>
+            <label className='new-item'>
+                <input className='all-state' name={id} type='checkbox' defaultChecked={done} onChange={onChange} />
+                <div className='all-text'>{Description}</div>
+            </label>
+        </Fragment>
     )
 }
 
